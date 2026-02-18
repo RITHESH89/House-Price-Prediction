2c5d2973f5e9cfaf1b2388e404de7488826f5b6a
Rithesh <ritheshasowda@gmail.com>
2026-02-18 11:47:07 +0530

    Update train_model.py

--- house-price-prediction/src/train_model.py
+++ house-price-prediction/src/train_model.py
@@ -1,11 +1,11 @@
 import numpy as np
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+from xgboost import XGBRegressor
 from preprocessing import preprocess_data
 
-# Load & preprocess
+# Load data
 X, y = preprocess_data("data/train.csv")
 
 # Split data
@@ -13,18 +13,27 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Train model
-model = RandomForestRegressor(
-    n_estimators=200,
+# XGBoost model
+model = XGBRegressor(
+    n_estimators=1000,
+    learning_rate=0.05,
+    max_depth=4,
+    subsample=0.8,
+    colsample_bytree=0.8,
     random_state=42
 )
+
 model.fit(X_train, y_train)
 
 # Predict
 predictions = model.predict(X_test)
 
+# Convert back from log scale
+predictions = np.expm1(predictions)
+y_test_original = np.expm1(y_test)
+
 # Evaluate
-rmse = np.sqrt(mean_squared_error(y_test, predictions))
+rmse = np.sqrt(mean_squared_error(y_test_original, predictions))
 print("RMSE:", rmse)
 
 # Save model
